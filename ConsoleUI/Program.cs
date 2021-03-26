d34009aa57295c32f5ea8e7b1722c52bd3d5147d
DESKTOP-J343IEJ\akyus <vmazman@gmail.com>
2021-03-26 23:54:16 +0100

    Lecture's End: ProductDto completed

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,8 +9,15 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //ProductTest();
+            //DTO:Data Transformation Object
+            ProductTest();
             //IoC
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
 
             foreach (var category in categoryManager.GetAll())
@@ -23,9 +30,9 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40, 100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName+" / "+product.CategoryName);
             }
         }
     }
